17585c8f8bd03f5ea61466bee828843cfcefda00
Evert Moreno Serrate <evert23122004@gmail.com>
2025-03-21 16:42:05 -0400

    Now redirects to home!

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 //import Button from './components/Button';
 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import HomePages from './pages/HomePages';
 import RegisterPage from './pages/RegisterPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -12,6 +12,7 @@ function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Navigate to="/home"/>} />
         <Route path='/home' element={<HomePages/>}/>
         <Route path='/register' element= {<RegisterPage/>}/>
         <Route path='/my-account' element= {<MyAccountPage/>}/>
