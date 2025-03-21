836db9fef732c238e2bb033604b2162d67351bb3
jonathan <rochacontrerasjonathan40@gmail.com>
2025-03-21 11:05:34 -0300

    Barra de busqueda

--- src/pages/HomePages.jsx
+++ src/pages/HomePages.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import { Navbar, Container, Nav } from 'react-bootstrap';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 //npm install bootstrap si no lo tienen instalado
 // verificar si está instalado
 //vnpm list react-bootstrap
+//npm install @fortawesome/react-fontawesome --save
+//npm install @fortawesome/free-solid-svg-icons --save
+
 
 const HomePages = () => {
     let nombre= "Jonathan";
@@ -28,21 +33,31 @@ const HomePages = () => {
 //export default HomePages
 
 const NavigationBar = () => {
-    return (
-      <Navbar bg="dark" variant="dark" expand="lg">
-        <Container>
-          <Navbar.Brand href="#home">Mi Sitio</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg" className="px-3">
+      <Container>
+        <Navbar.Brand href="#home">Mi Sitio</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />     
+        <Navbar.Collapse id="basic-navbar-nav" className="w-100">
+
+          <div className="d-flex w-100 justify-content-between align-items-center">
+            <div className="containerInput mx-auto d-flex align-items-center">
+              <input className="form-control inputBuscar" placeholder="Buscar" />
+              <button className="btn btn-success ms-2">
+                <FontAwesomeIcon icon={faSearch} />
+              </button>
+            </div>
+
+            <Nav>
               <Nav.Link href="#account">Mi Cuenta</Nav.Link>
               <Nav.Link href="#configurations">Configuraciones</Nav.Link>
               <Nav.Link href="#preferences">Preferencias</Nav.Link>
             </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    );
+          </div>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
   };
   
   export default NavigationBar;
